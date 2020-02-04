061bb704bcdef89c43e4589c3e36ea33f0efc1db
Michael Pankov <work@michaelpankov.com>
2020-02-04 12:34:01 +0300

    Remove issues from code
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-services/iml-devices/src/db.rs
+++ iml-services/iml-devices/src/db.rs
@@ -511,11 +511,6 @@ pub async fn update_virtual_devices<'a>(
 
 #[cfg(test)]
 mod test {
-// Current issues:
-// 1. Multiple updates for single device host
-// 2. No removal of device hosts
-// 3. Repetitive code of tests
-
     use super::*;
     use insta::assert_debug_snapshot;
     use std::{fs, path::Path};
